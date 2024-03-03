50f85624fb6dc6a56cdf3ccb5566ac78ba7d5046
Nikita Nikita <jassie.jassie@yandex.ru>
2024-03-03 13:40:11 +0300

    QuickSort Быстрая сортировка Хоара

--- ModuleApp12/Program.cs
+++ ModuleApp12/Program.cs
@@ -4,37 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        var numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        var inputArray = new int[] { 9, 12, 9, 4, 17, 5, 3, 6 };
 
-        int lowerBound = numbers.GetLowerBound(0);
-        int upperBound = numbers.GetUpperBound(0);
-
-        Console.Write("Введите число от 1 до 15 --> ");
-        int.TryParse(Console.ReadLine(), out int input);
-        
-        var result = BinarySearch(input, numbers, lowerBound, upperBound);
-
-        Console.WriteLine($"Индекс запрошенного элемента: { result}");
+        int[] sortedArray = QuickSort(inputArray, inputArray.GetLowerBound(0), inputArray.GetUpperBound(0));
+        Console.WriteLine($"Отсортированный массив: {string.Join(' ', sortedArray)}");
     }
 
-    static int BinarySearch(int desiredElement, int[] array, int lowerBound, int upperBound)
+    private static int[] QuickSort(int[] array, int minIndex, int maxIndex)
     {
-        var middle = (lowerBound + upperBound) / 2;
+        if(minIndex >= maxIndex)
+            return array;
 
-        if (array[middle] == desiredElement)
-            return middle;
+        int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
 
-        if(lowerBound < upperBound)
+        QuickSort(array, minIndex, pivotIndex - 1);
+        QuickSort(array, pivotIndex + 1, maxIndex);
+        
+        return array;
+    }
+
+    private static int GetPivotIndex(int[] array, int minIndx, int maxIndx)
+    {
+        int pivot = minIndx - 1;
+
+        for(int i = minIndx; i <= maxIndx; i++)
         {
-            if (desiredElement < array[middle])
-                return BinarySearch(desiredElement, array, lowerBound, middle - 1);
-
-            return BinarySearch(desiredElement, array, middle + 1, upperBound);
+            if (array[i] < array[maxIndx])
+            {
+                pivot++;
+                Swap(ref array[pivot], ref array[i]);
+            }
         }
 
-        return -1;
-            
+        pivot++;
+        Swap(ref array[pivot], ref array[maxIndx]);
 
+        return pivot;
+    }
 
+    private static void Swap(ref int leftItem, ref int  rightItem)
+    {
+        int temp = leftItem;
+
+        leftItem = rightItem;
+
+        rightItem = temp;
     }
 }
